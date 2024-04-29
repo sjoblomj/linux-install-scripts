4d0ebc14d6ea852fe7f22f4fcc3e18e386ffe830
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-04-29 19:41:21 +0200

    Replace bash with zsh ⚒️

--- 03_terminal/configure_fzf.sh
+++ 03_terminal/configure_fzf.sh
@@ -5,7 +5,7 @@ add_cronjob_to_check_git_repository "$HOME/bin/fzf"
 
 if [ -f $HOME/.fzf.bash ] && [ ! -f $HOME/.fzf.zsh ]; then
     cp $HOME/.fzf.bash $HOME/.fzf.zsh
-    sed -i 's/.bash"$/.zsh"/g' $HOME/.fzf.zsh
+    sed -i 's/bash/zsh/g' $HOME/.fzf.zsh
 fi
 if ! grep -sq ".fzf.zsh" $HOME/.zshrc ; then
     echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> $HOME/.zshrc

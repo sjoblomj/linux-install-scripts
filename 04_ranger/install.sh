d441bd2a5c8d70744f9334b872680f91060ebdcb
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-05-17 19:51:46 +0200

    Tidying up Ranger scope script 🌄

--- 04_ranger/install.sh
+++ 04_ranger/install.sh
@@ -14,9 +14,9 @@ sed -i "s|set preview_images false|set preview_images true|g" "$confdir"/ranger/
 sed -i "s|set preview_images_method .*|set preview_images_method ueberzug|g" "$confdir"/ranger/rc.conf
 sed -i "s|#set preview_script |set preview_script |g" "$confdir"/ranger/rc.conf
 
-# Download latest Rifle config. In Ranger <= 1.9.3, viewnior is not configured, but it is in the upstream source.
+# Download latest Rifle config.
 curl -so "$confdir"/ranger/rifle.conf https://raw.githubusercontent.com/ranger/ranger/master/ranger/config/rifle.conf
 
 # Enable video previewing
 ranger --copy-config=scope
-awk 'BEGIN{found_video = 0} {if ($0 ~ " *## Video$") found_video = 1; else if ($0 !~ ".*#.*") found_video = 0; if (found_video) sub("# ", "", $0); print $0;}' "$confdir"/ranger/scope.sh > "$confdir"/ranger/tmp.sh && mv "$confdir"/ranger/tmp.sh "$confdir"/ranger/scope.sh && chmod +x "$confdir"/ranger/scope.sh
+awk -i inplace 'BEGIN{found_video = 0} {if ($0 ~ " *## Video$") found_video = 1; else if ($0 !~ ".*#.*") found_video = 0; if (found_video) sub("# ", "", $0); print $0;}' "$confdir"/ranger/scope.sh && chmod +x "$confdir"/ranger/scope.sh

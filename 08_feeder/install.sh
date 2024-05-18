4d62f7ee8b00b5f2863eb569c8e338decd858e66
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-05-18 12:51:02 +0200

    Silencing git changes on the sites.yaml file 🤫

--- 08_feeder/install.sh
+++ 08_feeder/install.sh
@@ -3,4 +3,7 @@
 if [ ! -d $HOME/code/feeder ]; then
     git clone https://github.com/sjoblomj/feeder.git $HOME/code/feeder
     cp sites.yaml $HOME/code/feeder
+    cd $HOME/code/feeder || exit 1
+    git update-index --assume-unchanged sites.yaml
+    cd - || exit 1
 fi

d9adf39d19793d6de82259c0b62216fbc622e25f
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-06-06 22:29:13 +0200

    Escaping path 🛣️

--- 00_config/run.sh
+++ 00_config/run.sh
@@ -38,7 +38,7 @@ mkdir -p "$confdir"/labwc
 cd "$HOME"/code/arch-install-scripts/00_config || exit 1
 cp autostart environment menu.xml rc.xml themerc-override "$confdir"/labwc
 cp .zprofile "$HOME"/
-sed -i "s|./bin/labwc/build/labwc|$labwc_path/build/labwc|" "$HOME"/.zprofile
+sed -i "s|./bin/labwc/build/labwc|${labwc_path// /\\\\ }/build/labwc|" "$HOME"/.zprofile
 sed -i "s|\$HOME/.config|$confdir|g" "$confdir"/labwc/autostart
 
 

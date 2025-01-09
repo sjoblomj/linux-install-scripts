99eb9140c07e9d3e0954f742c2414f15377fed67
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-01-09 19:22:08 +0100

    Making install_script.sh work with Ubuntu 🐧

--- install_script.sh
+++ install_script.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+source common/install.sh
 
-sudo pacman-key --init
-sudo pacman-key --populate archlinux
-
-sudo pacman -Suy
-sudo pacman -S --needed git
+if [ $(is_ubuntu) ]; then
+	sudo apt-get update
+	sudo apt-get upgrade
+else
+	sudo pacman-key --init
+	sudo pacman-key --populate archlinux
+	sudo pacman -Suy
+fi
+install_programs curl git
 
 if [ -d ".git/hooks" ]; then
-  cp commit-msg .git/hooks
-  chmod +x .git/hooks/commit-msg
+	cp commit-msg .git/hooks
+	chmod +x .git/hooks/commit-msg
 fi
 
-git clone https://github.com/junegunn/fzf.git $HOME/bin/fzf
-cd $HOME/bin/fzf
+git clone https://github.com/junegunn/fzf.git "$HOME"/bin/fzf
+cd "$HOME"/bin/fzf
 ./install
-PATH="${PATH:+${PATH}:}$HOME/bin/fzf/bin"
+PATH="${PATH:+${PATH}:}"$HOME"/bin/fzf/bin"
 
-git clone https://github.com/sjoblomj/linux-install-scripts $HOME/code/linux-install-scripts
-cd $HOME/code/linux-install-scripts
+git clone https://github.com/sjoblomj/linux-install-scripts "$HOME"/code/linux-install-scripts
+cd "$HOME"/code/linux-install-scripts
 
 ./find_scripts.sh

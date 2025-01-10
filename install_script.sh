47ea4b38292f67b28a81c2f88b9b4e44ff7a2bef
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-01-10 09:04:11 +0100

    Making install_script.sh stand on its own legs 👮‍♀️

--- install_script.sh
+++ install_script.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-source common/install.sh
+
+is_ubuntu() {
+    if [[ $(awk -F= '/^NAME/{gsub("\"", "", $2); print $2}' /etc/os-release) = "Ubuntu" ]]; then
+        echo 1
+    else
+        echo 0
+    fi
+}
 
 if [ $(is_ubuntu) ]; then
 	sudo apt-get update
 	sudo apt-get upgrade
+    sudo apt-get install curl git
 else
 	sudo pacman-key --init
 	sudo pacman-key --populate archlinux
 	sudo pacman -Suy
-fi
-install_programs curl git
-
-if [ -d ".git/hooks" ]; then
-	cp commit-msg .git/hooks
-	chmod +x .git/hooks/commit-msg
+    sudo pacman -S --needed curl git
 fi
 
 git clone https://github.com/junegunn/fzf.git "$HOME"/bin/fzf
@@ -23,5 +26,11 @@ PATH="${PATH:+${PATH}:}"$HOME"/bin/fzf/bin"
 
 git clone https://github.com/sjoblomj/linux-install-scripts "$HOME"/code/linux-install-scripts
 cd "$HOME"/code/linux-install-scripts
+
+if [ -d ".git/hooks" ]; then
+	cp commit-msg .git/hooks
+	chmod +x .git/hooks/commit-msg
+fi
+
 
 ./find_scripts.sh

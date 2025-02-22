f6ae2ffae1ad78d5fb4780f20834e354d0da4b52
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-02-22 13:22:23 +0100

    Improving JetBrains install script 🏞️

--- 10_ides/install.sh
+++ 10_ides/install.sh
@@ -5,30 +5,40 @@ datadir="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 product_code="$1"
 product_dir="$2"
-if [ ! -d $HOME/bin/${product_dir}-* ]; then
+if ! ls "$HOME"/bin/"$product_dir"-* 1>/dev/null 2>&1 ; then
 	install_programs curl tar
 	install_programs xwayland  # No native Wayland support yet   https://youtrack.jetbrains.com/issue/JBR-3206
-	curl -L "https://download.jetbrains.com/product?code=${product_code}&latest&distribution=linux" -o "${product_code}.tar.gz"
+	curl -L "https://download.jetbrains.com/product?code=${product_code}&latest&distribution=linux" -o "$product_code".tar.gz
 
-	mkdir -p $HOME/bin
-	tar xvf "${product_code}.tar.gz" --directory=$HOME/bin
-	rm "${product_code}.tar.gz"
+	mkdir -p "$HOME/bin"
+	tar xvf "$product_code".tar.gz --directory="$HOME/bin"
+	rm "$product_code".tar.gz
 
-	jbdatadir="$confdir"/JetBrains/$(grep -Po "\"jbdatadirectoryName\": \"\K.+(?=\",)" $HOME/bin/${product_dir}-*/product-info.json)
+	jbconfdirname=$(grep -Po "^ *\"dataDirectoryName\": \"\K.+(?=\",)" "$HOME"/bin/"$product_dir"-*/product-info.json)
+	jbdatadir="$confdir"/JetBrains/"$jbconfdirname"
 	mkdir -p "$jbdatadir/options/linux"
 	mkdir -p "$jbdatadir/keymaps"
 
-	cp options_editor.xml "$jbdatadir/options/editor.xml"
-	cp options_editor-font.xml "$jbdatadir/options/editor-font.xml"
-	cp options_keymapFlags.xml "$jbdatadir/options/keymapFlags.xml"
-	cp options_ui-datetime.xml "$jbdatadir/options/ui-datetime.xml"
+	cp options_editor.xml       "$jbdatadir/options/editor.xml"
+	cp options_editor-font.xml  "$jbdatadir/options/editor-font.xml"
+	cp options_keymapFlags.xml  "$jbdatadir/options/keymapFlags.xml"
+	cp options_ui-datetime.xml  "$jbdatadir/options/ui-datetime.xml"
 	cp options_linux_keymap.xml "$jbdatadir/options/linux/keymap.xml"
 	cp syzygy.xml "$jbdatadir/keymaps"
 
-	# Create Desktop Entry and icons
+	# Create Desktop Entry
 	prod="${product_dir,,}"
-	sudo cp -s $HOME/bin/${product_dir}-*/bin/${prod}.sh /usr/local/bin/${prod}
-	sudo cp jetbrains-${prod}.desktop /usr/share/applications
-	mkdir -p "$datadir"/icons/hicolor/scalable/apps
-	cp $HOME/bin/${product_dir}-*/bin/${prod}.svg "$datadir"/icons/hicolor/scalable/apps/jetbrains-${prod}.svg
+	sudo cp -s "$HOME"/bin/"$product_dir"-*/bin/"$prod".sh /usr/local/bin/"$prod"
+	sudo cp jetbrains-"$prod".desktop /usr/share/applications
+
+    # Put icons in their right place
+	if [ $(is_ubuntu) -eq 1 ]; then
+		icontype="png"
+		iconsize="128x128"
+	else
+		icontype="svg"
+		iconsize="scalable"
+	fi
+	mkdir -p "$datadir"/icons/hicolor/"$iconsize"/apps
+	cp "$HOME"/bin/"$product_dir"-*/bin/"$prod"."$icontype" "$datadir"/icons/hicolor/"$iconsize"/apps/jetbrains-"$prod"."$icontype"
 fi

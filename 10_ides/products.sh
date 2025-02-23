df50d48e044bf2ee77d425ce091ad5a4e337b7c0
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-02-23 11:05:51 +0100

    Adding RustRover 🐶

--- 10_ides/products.sh
+++ 10_ides/products.sh
@@ -5,21 +5,25 @@ product_codes+=("IIU")
 product_codes+=("GO")
 product_codes+=("PCP")
 product_codes+=("CL")
+product_codes+=("RR")
 
 declare -A product_names
 product_names[IIU]="IntelliJ IDEA"
 product_names[GO]="GoLand"
 product_names[PCP]="PyCharm"
 product_names[CL]="CLion"
+product_names[RR]="RustRover"
 
 declare -A product_desc
 product_desc[IIU]="Java and Kotlin"
 product_desc[GO]="Go"
 product_desc[PCP]="Python"
 product_desc[CL]="C and C++"
+product_desc[RR]="Rust"
 
 declare -A product_dir
 product_dir[IIU]="idea"
 product_dir[GO]="GoLand"
 product_dir[PCP]="pycharm"
 product_dir[CL]="clion"
+product_dir[RR]="RustRover"

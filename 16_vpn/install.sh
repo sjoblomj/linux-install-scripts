ed3892cceea94151466402f03e452b796c18a731
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-03-03 20:27:10 +0100

    Adding Mullvad VPN 🦡

---
+++ 16_vpn/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ../common/install.sh
+
+if [ $(is_ubuntu) -eq 1 ]; then
+	echo "Automatic setup is not available for Ubuntu"
+else
+	install_programs openresolv wireguard-tools
+
+	sudo cp se-got-wg-004.conf /etc/wireguard
+
+	sudo systemctl enable wg-quick@se-got-wg-004.service
+	sudo systemctl start  wg-quick@se-got-wg-004.service
+
+	# Make WireGuard resume after sleep
+	# https://wiki.archlinux.org/title/WireGuard#Connection_lost_after_sleep_using_systemd-networkd
+	sudo sed -E -i 's/^#?ManageForeignRoutingPolicyRules=(yes|no)$/ManageForeignRoutingPolicyRules=no/' /etc/systemd/networkd.conf
+	sudo systemctl restart systemd-networkd
+fi

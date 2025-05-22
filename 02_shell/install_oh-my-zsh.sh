fddbd45a427a8fb41c702761eaddea2fe0982dcc
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-05-23 00:14:46 +0200

    Adding exa and fixing code warnings 🇩🇰

--- 02_shell/install_oh-my-zsh.sh
+++ 02_shell/install_oh-my-zsh.sh
@@ -2,7 +2,7 @@
 source ../common/aliases.sh
 source ../common/install.sh
 
-if [ $(is_ubuntu) -eq 1 ]; then
+if [ "$(is_ubuntu)" -eq 1 ]; then
 	install_programs zsh
 	install_programs gawk
 else
@@ -11,8 +11,8 @@ fi
 install_programs curl
 
 if [[ -z $ZSH ]]; then
-	sudo chsh -s $(which zsh) # Change default shell for root user
-	chsh -s $(which zsh)      # Change default shell for normal user
+	sudo chsh -s "$(which zsh)" # Change default shell for root user
+	chsh -s "$(which zsh)"      # Change default shell for normal user
 
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -23,13 +23,18 @@ if [[ -z $ZSH ]]; then
 	if ! grep -sq "bgnotify_formatted" "$HOME"/.zshrc ; then
 		plugins="git bgnotify sudo"
 		# Use the bgnotify custom command from the bgnotify readme, but modify it
-		bgnotify_func=$(cat "$HOME"/.oh-my-zsh/plugins/bgnotify/README.md | awk 'BEGIN{In_function = 0}{if ($0 == "function bgnotify_formatted {") In_function = 1; if (In_function) print $0; if ($0 == "}") In_function = 0}' | sed "s|Holy Smokes Batman|Great success|; s|Holy Graf Zeppelin|Command failed|; s|\$HOME/icons|\$HOME/$iconpath|g")
+		bgnotify_func=$(awk 'BEGIN{In_function = 0}{if ($0 == "function bgnotify_formatted {") In_function = 1; if (In_function) print $0; if ($0 == "}") In_function = 0}' "$HOME"/.oh-my-zsh/plugins/bgnotify/README.md | sed "s|Holy Smokes Batman|Great success|; s|Holy Graf Zeppelin|Command failed|; s|\$HOME/icons|\$HOME/$iconpath|g")
 
 		awk -v plugins="$plugins" -v bgnotify_func="${bgnotify_func}" -i inplace '{if ($0 ~ /^plugins=(.*)$/) print bgnotify_func "\nplugins=(" plugins ")"; else print $0}' "$HOME"/.zshrc
 	fi
 
-	make_alias "alias l='ls  -la --group-directories-first'"
-	make_alias "alias ll='ls -la --group-directories-first'"
+	if ! command -v eza >/dev/null 2>&1 ; then
+		make_alias "alias l='ls  -la --group-directories-first'"
+		make_alias "alias ll='ls -la --group-directories-first'"
+	else
+		make_alias "alias l='eza  -la --icons=always --time-style=long-iso --git --git-repos'"
+		make_alias "alias ll='eza -la --icons=always --time-style=long-iso --git --git-repos'"
+	fi
 	make_alias "alias weather='curl wttr.in\?M'"
 	make_alias "alias distory='vim $HOME/.zsh_history'"
 	make_alias "alias dedup='find . ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD'"

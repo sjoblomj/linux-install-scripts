b2bdb755e54fa4a40383248a14fbf0c08715905e
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-07-12 08:32:57 +0200

    Added git alias 🎋

--- 05_git/install.sh
+++ 05_git/install.sh
@@ -27,5 +27,6 @@ git config --global alias.br branch
 git config --global alias.cp cherry-pick
 git config --global alias.l  "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(red)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
 git config --global alias.l2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(red)- %an%C(reset)'"
+git config --global alias.w  "\!git branch --sort=-committerdate | fzf --preview 'git show --color=always {-1}' --bind 'enter:become(git checkout {-1})' --layout reverse"
 
 make_alias "alias gits='git status'"

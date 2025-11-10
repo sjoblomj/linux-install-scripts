e41010b28a4ac2fc87b6670d7fb8dc1ad00e2dc4
Johan Sjöblom <sjoblom.johan@gmail.com>
2025-11-10 20:34:48 +0100

    Chromium no longer needs annoying flags to run on Wayland 🔨

--- 04_browser/install_chromium.sh
+++ 04_browser/install_chromium.sh
@@ -8,7 +8,6 @@ if [ "$(is_ubuntu)" -eq 1 ]; then
 fi
 
 install_programs "$chromium"
-sudo sed -i "s|^Exec=/usr/bin/chromium|Exec=/usr/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland|g" /usr/share/applications/"$chromium".desktop
 
 mkdir -p "$HOME"/.local/share/icons/hicolor/scalable/apps
 cp chromium.svg "$HOME/.local/share/icons/hicolor/scalable/apps/chromium.svg"
